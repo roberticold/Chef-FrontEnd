8e3716852b4e60d71df5c32a3734a787ce3d18bf
roberticold <70712597+roberticold@users.noreply.github.com>
2021-06-20 15:58:41 -0500

    changes

--- src/components/Editrecipe.jsx
+++ src/components/Editrecipe.jsx
@@ -296,8 +296,8 @@ export default class Editrecipe extends Component {
                   </div>
                 </div>
                  </div>
-      <div className="row justify-content-center m-2">
-        <div className="col col-xxl-8 col-xl-8 col-lg-8 col-md-8 col-sm-8 col-xs-8 col-xxs-8">
+                 <div className="row justify-content-center m-3">
+                <div className="col-xxl-6 col-xl-6 col-lg-8 col-md-12 col-sm-12 col-xs-12">
                 <div className="card shadow-sm ">
                     <h5>Comments</h5>
                    
