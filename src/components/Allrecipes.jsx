71e9ca1ef7be16c2ea9e3b0c8825d945a7db935e
roberticold <70712597+roberticold@users.noreply.github.com>
2021-06-20 22:41:11 -0500

    changes

--- src/components/Allrecipes.jsx
+++ src/components/Allrecipes.jsx
@@ -42,7 +42,7 @@ export default class Allrecipes extends Component {
   
   
 
-  <div className="row justify-content-center  m-2">
+  <div className="row justify-content-center  m-3">
       <div className="col-xxl-6 col-xl-6 col-lg-8 col-md-12 col-sm-12 col-xs-12">
         
             <div className="card shadow-sm" id="radius">
